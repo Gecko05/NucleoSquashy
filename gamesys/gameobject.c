680a89db10b9cc9aac0e814d2d9991a8eba17c05
gecko05 <jacacent@gmail.com>
2019-02-09 18:16:43 -0600

    from workspace

--- gamesys/gameobject.c
+++
@@ -1,8 +0,0 @@
-#include "gameobject.h"
-
-/*bodyNode vCreateObj(spriteArray spriteArray){
-	bodyNode newObj = malloc(sizeof(*newObj));
-	newObj->sprite = spriteArray;
-	newObj->x = newObj->y = 0;
-	return newObj;
-}*/
